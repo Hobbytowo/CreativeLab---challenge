d395c68fe9e1873e5a5191f00c30f4ab7f063bfb
Hobbytowo <weronika.ak@wp.pl>
2018-10-14 22:20:39 +0200

    add dynamic changing main article on list item click and change templates article content

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,32 @@
+// dynamic changing main article on list item click
 
+const listOfArticles = document.querySelector('.list--articles')
+const listItems = document.querySelectorAll('.main__listItem')
+const articles = document.querySelectorAll('.main__article')
+
+listOfArticles.addEventListener('click', e => {
+  const listElement = e.target
+
+  if (listElement.className.includes('listItem--active')) {
+    return // if click on active element
+  }
+
+  const articleName = listElement.href.split('#')[1]
+  const articleToShow = document.querySelector(`.article--${ articleName }`)
+
+  // remove articles active class
+  articles.forEach(article => {
+    article.classList.remove('article--active')
+  })
+
+  // add article active class
+  articleToShow.classList.add('article--active')
+
+  // remove list items active class
+  listItems.forEach(listItem => {
+    listItem.classList.remove('listItem--active')
+  })
+
+  // add list items active class
+  listElement.classList.add('listItem--active')
+})
