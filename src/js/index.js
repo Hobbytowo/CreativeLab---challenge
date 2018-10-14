b4d3e6af48ff1379481d19ab55d35a8158803340
Hobbytowo <weronika.ak@wp.pl>
2018-10-14 23:32:14 +0200

    add dynamic changing article image

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,32 @@
 // dynamic changing main article on list item click
-
 const listOfArticles = document.querySelector('.list--articles')
 const listItems = document.querySelectorAll('.main__listItem')
 const articles = document.querySelectorAll('.main__article')
+const imgWrapper = document.querySelector('.main__imgWrapper')
 
+// fn addImage depend on window width
+const addImage = (name, right = 0, top = 0) => {
+  if (name === 'templates') {
+    right = 60
+    top = 80
+  }
+
+  if (window.innerWidth > 1300) {
+    imgWrapper.style.background =
+      `url("static/${ name }XL.png") no-repeat ${ right }px ${ top }px`
+  } else if (window.innerWidth > 1070) {
+    imgWrapper.style.background =
+      `url("static/${ name }L.png") no-repeat ${ right }px ${ top }px`
+  } else if (window.innerWidth > 515) {
+    imgWrapper.style.background =
+      `url("static/${ name }M.png") no-repeat ${ right }px ${ top }px`
+  } else {
+    imgWrapper.style.background =
+      `url("static/${ name }S.png") no-repeat ${ right }px ${ top }px`
+  }
+}
+
+// click event listener
 listOfArticles.addEventListener('click', e => {
   const listElement = e.target
 
@@ -29,4 +52,14 @@ listOfArticles.addEventListener('click', e => {
 
   // add list items active class
   listElement.classList.add('listItem--active')
+
+  // change article imaage background
+  addImage(articleName)
+})
+
+// resize event listener
+window.addEventListener('resize', () => {
+  const activeArticleName = document.querySelector('.listItem--active').href.split('#')[1]
+
+  addImage(activeArticleName)
 })
